78576f3ec62a16d31221ca5c9dfbe43625095df3
Sridar Madurai <srieee284@gmail.com>
2025-11-03 13:14:35 -0800

    Adding unit test for helper function for summary

--- src/utils/__tests__/helperFunctions.test.js
+++ src/utils/__tests__/helperFunctions.test.js
@@ -1,4 +1,4 @@
-import { calculatePoints } from "../helperFunctions";
+import { calculatePoints, summarizePoints } from "../helperFunctions";
 
 describe("calculatePoints", () => {
   it("returns 0 for purchases <= 50", () => {
@@ -14,5 +14,41 @@ describe("calculatePoints", () => {
   it("returns 2 points per dollar over 100 + 50 points for 50 - 100 tier", () => {
     expect(calculatePoints(120)).toBe(90);
     expect(calculatePoints(200)).toBe(250);
+  });
+});
+
+describe("summarizePoints", () => {
+  const mockTxns = [
+    {
+      amount: 120,
+      date: "2025-08-10T00:00:00Z",
+      customer: { name: "Mason Thomas" },
+    },
+    {
+      amount: 200,
+      date: "2025-08-15T00:00:00Z",
+      customer: { name: "Mason Thomas" },
+    },
+    {
+      amount: 90,
+      date: "2025-09-05T00:00:00Z",
+      customer: { name: "Mason Thomas" },
+    },
+    {
+      amount: 150,
+      date: "2025-09-10T00:00:00Z",
+      customer: { name: "Lucas Perez" },
+    },
+  ];
+
+  it("groups by customer + month and sums correctly", () => {
+    const result = summarizePoints(mockTxns);
+    expect(result["Mason Thomas"]).toBeDefined();
+    expect(result["Lucas Perez"]).toBeDefined();
+
+    expect(result["Mason Thomas"].months["August 2025"]).toBeCloseTo(340.0, 2);
+    expect(result["Lucas Perez"].months["September 2025"]).toBeCloseTo(150.0, 2);
+
+    expect(result["Mason Thomas"].total).toBeCloseTo(380.0, 2);
   });
 });
